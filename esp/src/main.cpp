2e2a4d368aea9a818a07140f03e1e28548bc0e9b
jiA-AS <3936946610@qq.com>
2026-07-24 14:28:16 +0800

    无miro-ROS版本

--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -1,33 +1,23 @@
 /**
  * @file main.cpp
  * @author fishros (fishros@foxmail.com)
- * @brief 主函数部分
- * @version V1.0.0
- * @date 2023-01-04
- * 
+ * @brief 主函数部分（PLD2026 无ROS版：不再创建 micro-ROS 传输任务）
+ * @version V1.0.0-noros
+ *
  * @copyright Copyright (c) fishros.com & fishros.org.cn 2023
- * 
+ *
  */
 #include <Arduino.h>
 #include "fishbot.h"
 
-void fishbot_loop_transport_task(void *param)
-{
-  setup_fishbot_transport();
-  while (true)
-  {
-    loop_fishbot_transport();
-  }
-}
-
 void setup()
 {
   setup_fishbot();
-  xTaskCreatePinnedToCore(fishbot_loop_transport_task, "transport_task", 10240, NULL, 1, NULL, 0);
 }
 
 void loop()
 {
-  delay(10);
+  loop_fishbot_console(); // $command 串口控制台：read_config / restart / pid_kp 等
+  delay(10);              // 10ms -> loop_fishbot_control 以 100Hz 运行
   loop_fishbot_control();
 }
